3ffd2af6e3c2cd46c3b50152bb1ea286bdc87eed
harumhl <haru.mhl@gmail.com>
2017-08-02 01:22:29 -0400

    trying... connect-mongodb

--- src/message.js
+++ src/message.js
@@ -53,7 +53,7 @@ const replyMessage = (message) => {
             console.log("query_result")
             console.log(query_result)
             //.then(query_result => {
-            message.addReply(query_result['character_name'])
+            message.addReply({type: 'text', content: query_result['character_name']})
             console.log("name: " + query_result['character_name'])
             message.reply()
             //})
