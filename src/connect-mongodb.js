e4271bbb2bbbbcdef5b3b86b0f7217ef3262776d
harumhl <haru.mhl@gmail.com>
2017-08-02 00:54:36 -0400

    trying... connect-mongodb

--- src/connect-mongodb.js
+++ src/connect-mongodb.js
@@ -68,10 +68,10 @@ const connectAndFindDoc = (query, result_storage) => {
     var i = 0
     var theInterval = setInterval(function(){console.log(i)
                                 i = i + 1
-                                if(typeof query_result !== 'undefined' || i >= 25)
+                                if(typeof query_result !== 'undefined' || i >= 8) // 2 sec
                                     clearInterval(theInterval);
                                     console.log(query_result)
-                                  }, 100)
+                                  }, 250)
     return query_result
 }
 
