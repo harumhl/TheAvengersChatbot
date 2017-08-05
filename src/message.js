38874ee3daef11417b98a2400b1a8d0fa3f87c86
harumhl <haru.mhl@gmail.com>
2017-08-04 23:57:32 -0400

    trying... connect-mongodb

--- src/message.js
+++ src/message.js
@@ -46,15 +46,14 @@ const replyMessage = (message) => {
     message.addReply({type: 'text', content: "hi"})
     if (result.action && result.action.done) {
         if(result.action.slug === 'ask-facts-character-name') {
-            connectAndFindDoc({hero_name: result.getMemory('query-hero-name').raw})
-            .then(query_result => {
-                  console.log(query_result)
-                  console.log(query_result['character_name'])
-                  console.log(result)
-                  message.addReply({ type: 'text', content: query_result['character_name'] })
-                  message.addReply({ type: 'text', content: 'whatever' })
-                  message.addReply(query_result)
-            },console.error)
+            var query_result = connectAndFindDoc({hero_name: result.getMemory('query-hero-name').raw})
+        
+            console.log(query_result)
+            console.log(query_result[_result]['character_name'])
+            console.log(result)
+            message.addReply({ type: 'text', content: query_result[_result]['character_name'] })
+            message.addReply({ type: 'text', content: 'whatever' })
+            message.addReply(query_result)
         }
     }
         
